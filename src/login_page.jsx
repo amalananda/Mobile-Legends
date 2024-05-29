008f749ab4cb03bea6e23a5ce5dd83ea8d2b2a04
amalananda <amalananda332@gmail.com>
2024-05-29 18:01:04 +0700

    refac: set modal alert in login page

--- src/login_page.jsx
+++ src/login_page.jsx
@@ -1,45 +1,105 @@
 import { useState } from "react";
 import ListNavbar from "./listnavbar";
 import Footer from "./footer";
-import { Container, Button } from "reactstrap";
+import { Container, Button, Modal, ModalBody, ModalFooter, ModalHeader, Alert } from "reactstrap";
 import { Link } from "react-router-dom";
 
+const database = [
+  {
+    email: "kamal@g.com",
+    password: "wow",
+  },
+  {
+    email: "uduh@g.com",
+    password: "uduh",
+  },
+];
+
 const LoginPage = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const [isOpen, setIsOpen] = useState(false);
+  const [alertMessage, setAlertMessage] = useState("");
+
+  const handleClickToggle = () => {
+    setIsOpen(!isOpen);
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setError("");
+
+    // Simple validation
+    if (!email) {
+      setError("Email is required");
+      return;
+    }
+    if (!password) {
+      setError("Password is required");
+
+      return;
+    }
+
+    // Add more validation or API call here
+
+    // Dummy check
+
+    const account = database.find((account) => account.email == email);
+
+    if (account?.email && account?.password == password) {
+      alert("Login successful");
+    } else {
+      setError("Invalid email or password");
+      // alert("Invalid email or password");
+      setAlertMessage("Invalid email or password");
+      setIsOpen(true);
+    }
+  };
+
+  const ModalAction = () => (
+    <Modal isOpen={isOpen} toggle={handleClickToggle}>
+      <ModalBody style={{ backgroundColor: "#FFA27F" }}>{alertMessage}</ModalBody>
+    </Modal>
+  );
+
   return (
     <>
       <ListNavbar />
       <Container>
         <div className="form-log pt-3">
-          <div className="col-lg-4">
+          <div className="col-lg-4 mx-auto">
             <div className="login-box">
               <header>Login</header>
+              <ModalAction />
               <div className="login-header"></div>
-              <div className="input-box">
-                <input type="text" className="input-field" placeholder="Email" autoComplete="off" required></input>
-              </div>
-              <div className="input-box">
-                <input type="password" className="input-field" placeholder="Password" autoComplete="off" required></input>
-              </div>
-              <div className="forgot">
-                <section>
-                  <input type="checkbox" id="check"></input>
-                  <label for="check">Remember Me</label>
-                </section>
-                <section>
-                  <a href="#">Forgot password ?</a>
-                </section>
-              </div>
-              <div className="input-submit">
-                <button className="submit-btn" id="submit"></button>
-                <label for="submit">
-                  <Link style={{ textDecoration: "none", color: "white" }} to={`/`}>
+              {/* {error && <div className="error">{error}</div>} */}
+              <form onSubmit={handleSubmit}>
+                <div className="input-box">
+                  <input type="text" className="input-field" placeholder="Email" autoComplete="off" value={email} onChange={(e) => setEmail(e.target.value)} required />
+                </div>
+                <div className="input-box">
+                  <input type="password" className="input-field" placeholder="Password" autoComplete="off" value={password} onChange={(e) => setPassword(e.target.value)} required />
+                </div>
+                <div className="forgot">
+                  <section>
+                    <input type="checkbox" id="check" />
+                    <label htmlFor="check">Remember Me</label>
+                  </section>
+                  <section>
+                    <a href="#">Forgot password?</a>
+                  </section>
+                </div>
+                <div className="input-submit">
+                  <button type="submit" className="submit-btn">
+                    {/* <Link style={{ textDecoration: "none", textDecorationColor: "white" }} to={`/`}> */}
                     Sign In
-                  </Link>
-                </label>
-              </div>
+                    {/* </Link> */}
+                  </button>
+                </div>
+              </form>
               <div className="sign-up-link">
                 <Link style={{ textDecoration: "none", color: "black" }} to={`/sign_up`}>
-                  Don't have account ?
+                  Don't have an account?
                 </Link>
               </div>
             </div>
@@ -50,4 +110,5 @@ const LoginPage = () => {
     </>
   );
 };
+
 export default LoginPage;
